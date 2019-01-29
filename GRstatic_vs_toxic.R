c7101a937e4a6dacb31dacc1fcb43c3e3e986926
Nicholas Clark <clarkno@mail.uc.edu>
2019-01-29 11:24:15 -0500

    update variable names

--- GRstatic_vs_toxic.R
+++ GRstatic_vs_toxic.R
@@ -3,17 +3,17 @@
   ### see lines 152-213 in https://github.com/datarail/DrugResponse/blob/1d0fd1898e797b74f611eb61dbd6926ac715bb83/MATLAB/import_columbus/Process_CellCountData2.m
   
   #inputData = readr::read_csv("/Users/nicholasclark/Desktop/Git/grmetrics_resources/GR_v2_all.csv")
-  ### Note: what about NA values in Deadcount and Cellcount?? these will propagate currently, but we may want to set them to 0 instead?
+  ### Note: what about NA values in dead_count and cell_count?? these will propagate currently, but we may want to set them to 0 instead?
   inputData %<>% 
-    dplyr::mutate(too_few = Deadcount + Cellcount < .95*(Day0Cnt + Day0DeadCnt)) %>%
+    dplyr::mutate(too_few = dead_count + cell_count < .95*(cell_count__time0 + dead_count__time0)) %>%
     dplyr::mutate(too_many = pert_type != 'ctl_vehicle' & 
-        Deadcount + Cellcount > 1.15*(Ctrlcount + Ctrl_Deadcount))
+        dead_count + cell_count > 1.15*(cell_count__ctrl + dead_count__ctrl))
   ## if missing cells, assign to dead cells
-  inputData %<>% dplyr::mutate( Deadcount = ifelse(!too_few, Deadcount, 
-    pmax(Deadcount, Day0Cnt + Day0DeadCnt - floor(.95*Cellcount) + 1, na.rm = T)))
+  inputData %<>% dplyr::mutate( dead_count = ifelse(!too_few, dead_count, 
+    pmax(dead_count, cell_count__time0 + dead_count__time0 - floor(.95*cell_count) + 1, na.rm = T)))
   # if too many dead cells -> subtract dead cells
-  inputData %<>% dplyr::mutate( Deadcount = ifelse(!too_many, Deadcount, 
-    pmin(Deadcount, Ctrlcount + Ctrl_Deadcount - ceiling(1.15*Cellcount) - 1, na.rm = T)))
+  inputData %<>% dplyr::mutate( dead_count = ifelse(!too_many, dead_count, 
+    pmin(dead_count, cell_count__ctrl + dead_count__ctrl - ceiling(1.15*cell_count) - 1, na.rm = T)))
   
   num_cond_true = sum(inputData$too_few == T, na.rm = T)
   num_cond_missing = sum(is.na(inputData$too_few))
@@ -26,18 +26,13 @@
     warning(msg)
   }
   
-  # Transform time to days for normalization of GR_d #### change this to time = "days" or "hours" # default = not changing
-  # if(max(inputData$Time, na.rm = TRUE) > 14) {
-  #   inputData %<>% dplyr::mutate( Time = Time/24 )
-  #   warning("Time transformed to days for normalization of GR_d")
-  # }
   ### how should we treat NAs here?? na.rm or not?
   inputData %<>% dplyr::mutate(
     # Dratio = (increase in dead cells, maxed out at 100%)/(increase in alive cells)
-    Dratio = pmax(Deadcount - Day0DeadCnt, 1, na.rm = T)/(Cellcount - Day0Cnt),
-    Dratio_ctrl = pmax(Ctrl_Deadcount - Day0DeadCnt, 1, na.rm = T)/(Ctrlcount - Day0Cnt),
-    gr = log2(Cellcount/Day0Cnt),
-    gr_ctrl = log2(Ctrlcount/Day0Cnt)
+    Dratio = pmax(dead_count - dead_count__time0, 1, na.rm = T)/(cell_count - cell_count__time0),
+    Dratio_ctrl = pmax(dead_count__ctrl - dead_count__time0, 1, na.rm = T)/(cell_count__ctrl - cell_count__time0),
+    gr = log2(cell_count/cell_count__time0),
+    gr_ctrl = log2(cell_count__ctrl/cell_count__time0)
     )
   ## These are slightly different from the formulas in the slide-deck, which are correct?
   inputData %<>% dplyr::mutate(
@@ -46,20 +41,20 @@
   )
 
   inputData %<>% dplyr::mutate(
-    #diff = abs(Cellcount - Day0Cnt)/Cellcount,
+    #diff = abs(cell_count - cell_count__time0)/cell_count,
     #Dratio_times_gr = gr*Dratio,
-    Eqidx = abs(Cellcount - Day0Cnt)/Cellcount < 1e-10
+    Eqidx = abs(cell_count - cell_count__time0)/cell_count < 1e-10
   )
   if(sum(inputData$Eqidx, na.rm = TRUE) > 0) {
-    warning("issue when Cellcount == Day0Cnt (approx.), denominator of Dratio is approx. zero -> use L'hospital's rule on Dratio*gr")
-    # a = Cellcount
-    # b = Day0Cnt
-    # d = pmax(Deadcount - Day0DeadCnt, 1, na.rm = T)
+    warning("issue when cell_count == cell_count__time0 (approx.), denominator of Dratio is approx. zero -> use L'hospital's rule on Dratio*gr")
+    # a = cell_count
+    # b = cell_count__time0
+    # d = pmax(dead_count - dead_count__time0, 1, na.rm = T)
     # Dratio*gr = d*log2(a/b)/(a-b)  ===> consider f(x) = d*log2(x/b)/(x-b) as x->b
     # lim x->b f(x) = lim x->b { d*log2(x/b)/(x-b) } = d/(b*log(2)) by L'hospital's rule
     inputData %<>% dplyr::mutate(
       Dratio_gr = ifelse(!Eqidx, NA,
-        pmax(Deadcount - Day0DeadCnt, 1, na.rm = T) /(Day0Cnt*log(2)) )
+        pmax(dead_count - dead_count__time0, 1, na.rm = T) /(cell_count__time0*log(2)) )
     )
     inputData %<>% dplyr::mutate(
       GR_s = ifelse(!Eqidx, GR_s,
@@ -74,27 +69,4 @@
     GR_naive = 2^(gr/gr_ctrl) - 1
   )
   return(inputData)
-
-  ##### taylor series approximation, translated from matlab code. 
-  # if(sum(inputData$Eqidx, na.rm = TRUE) > 0) {
-  #   warning("issue when Cellcount == Day0Cnt (approx.), denominator of Dratio is approx. zero -> Taylor series")
-  #   ##### not sure about the correctness of this part
-  #   # a = Cellcount
-  #   # b = Day0Cnt
-  #   inputData %<>% dplyr::mutate(
-  #     Dratio_gr = #ifelse(!Eqidx, NA,
-  #       pmax(Deadcount - Day0DeadCnt, 1, na.rm = T) *
-  #         (1/Day0Cnt + rowSums( sapply(1:35, function(x) {
-  #           Day0Cnt*x*((Day0Cnt-Cellcount)^x)
-  #           # b*n*(b-a)^n
-  #         }) ) ) #)
-  #   )
-  #   inputData %<>% dplyr::mutate(
-  #     GR_s = ifelse(!Eqidx, GR_s,
-  #                   2^((gr + Dratio_gr)/((1 + Dratio_ctrl)*gr_ctrl))-1),
-  #     GR_d = ifelse(!Eqidx, GR_d,
-  #                   2^(((Dratio_ctrl)*gr_ctrl - Dratio_gr)/Time)-1)
-  #   )
-  # }
-
 }  
